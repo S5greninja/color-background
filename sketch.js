0317a34b0224728dc618d4336b7601fa72e3c802
S5greninja <88627209+S5greninja@users.noreply.github.com>
2021-08-11 19:53:12 +0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,33 @@
+
+
+
+
+function draw() 
+{
+  createCanvas(600,200);
+ 
+  background(220);
+if(keyIsDown(RIGHT_ARROW))
+{
+box.x=box.x+5
+background("#ff0000")
+}
+if(keyIsDown(LEFT_ARROW))
+{
+box.x=box.x-5
+background("#b752ff")
+}
+if(keyIsDown(DOWN_ARROW))
+{
+box.x=box.x-5
+background("#00bbff")
+}
+if(keyIsDown(UP_ARROW))
+{
+box.x=box.x-5
+background("#04de25")
+}
+
+drawSprites() 
+}
+
